b58fe23961f12a70c9871de62ebf91ac74d019a7
Aashish Dhawan <aashishdhawan@gmail.com>
2021-12-23 10:00:02 +0530

    more UI issues fixed

--- app/controllers/screening/interview_controller.rb
+++ app/controllers/screening/interview_controller.rb
@@ -1,2 +1,6 @@
 class Screening::InterviewController < ApplicationController
+  def index
+    file = File.read("./json/interview.json")
+    @data = JSON.parse(file, object_class: Job)
+  end
 end
--- app/controllers/screening/resume_controller.rb
+++ app/controllers/screening/resume_controller.rb
@@ -1,6 +1,6 @@
 class Screening::ResumeController < ApplicationController
   def index
-    file = File.read("./resume.json")
+    file = File.read("./json/resume.json")
     @data = JSON.parse(file, object_class: Job)
   end
 end
