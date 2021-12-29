b7bbc64a79f7d3e783e09770594d2da09a81f1f1
hansika17 <hansika.kalra171@gmail.com>
2021-12-29 18:06:22 +0530

    devise invite implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable,  :timeoutable, timeout_in: 5.days
+         :recoverable, :rememberable, :validatable, :confirmable, :invitable, :timeoutable, timeout_in: 5.days,invite_for: 2.weeks
 
 
          validates_presence_of :first_name, :last_name, :email
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_132622) do
+ActiveRecord::Schema.define(version: 2021_12_29_114617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2021_12_26_132622) do
     t.boolean "winner"
     t.integer "score"
     t.string "comment"
-    t.datetime "created_at", precision: 6, default: "2021-12-24 12:39:55", null: false
-    t.datetime "updated_at", precision: 6, default: "2021-12-24 12:39:55", null: false
+    t.datetime "created_at", precision: 6, default: "2021-12-29 11:51:36", null: false
+    t.datetime "updated_at", precision: 6, default: "2021-12-29 11:51:36", null: false
   end
 
   create_table "survey_options", force: :cascade do |t|
@@ -125,8 +125,20 @@ ActiveRecord::Schema.define(version: 2021_12_26_132622) do
     t.string "confirmation_token"
     t.datetime "confirmed_at", precision: 6
     t.datetime "confirmation_sent_at", precision: 6
+    t.integer "permission", default: 0, null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at", precision: 6
+    t.datetime "invitation_sent_at", precision: 6
+    t.datetime "invitation_accepted_at", precision: 6
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
