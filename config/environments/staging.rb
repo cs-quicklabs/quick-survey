15a63fb7858024aa2e409a6d46abe197275cb912
Aashish Dhawan <aashishdhawan@gmail.com>
2022-01-09 15:12:34 +0530

    env config changed

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :digitalocean
+  config.active_storage.service = :local
   Rails.application.config.active_storage.variant_processor = :mini_magick
 
   # Mount Action Cable outside main process or domain.
