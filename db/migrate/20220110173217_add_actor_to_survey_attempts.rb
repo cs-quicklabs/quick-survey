bc66db343fd82841fef6106434db470b155cdf94
hansika17 <hansika.kalra171@gmail.com>
2022-01-13 19:08:02 +0530

    issues fixed

--- db/migrate/20220110173217_add_actor_to_survey_attempts.rb
+++ db/migrate/20220110173217_add_actor_to_survey_attempts.rb
@@ -1,5 +1,5 @@
 class AddActorToSurveyAttempts < ActiveRecord::Migration[7.0]
   def change
-    add_column :survey_attempts, :actor_id, :integer, null: true, default: ""
+    add_column :survey_attempts, :actor_id, :integer
   end
 end
