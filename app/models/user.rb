d69b766855cfc249320a7eca4db58b62854c4861
hansika17 <hansika.kalra171@gmail.com>
2023-08-09 19:29:42 +0530

    add new folder js fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :invitable, :timeoutable, timeout_in: 5.days, invite_for: 2.weeks
 
   enum role: [:member, :admin, :superadmin]
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name, on: :create
 
   ROLES = [
     ["Select a role", ""],
