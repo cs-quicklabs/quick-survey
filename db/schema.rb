9f0e595055fefc460bacb5179ed8afb54e620064
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-22 08:36:27 +0530

    fix issue with loop

--- db/schema.rb
+++ db/schema.rb
@@ -14,19 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "accounts", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
     t.bigint "record_id", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -34,7 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -45,22 +42,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "folders", force: :cascade do |t|
     t.string "title"
-    t.bigint "account_id"
     t.bigint "user_id", null: false
     t.bigint "space_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["account_id"], name: "index_folders_on_account_id"
     t.index ["space_id"], name: "index_folders_on_space_id"
     t.index ["user_id"], name: "index_folders_on_user_id"
   end
@@ -82,14 +79,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
 
   create_table "spaces", force: :cascade do |t|
     t.string "title"
-    t.bigint "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "description"
     t.boolean "archive", default: false
     t.datetime "archive_at"
-    t.index ["account_id"], name: "index_spaces_on_account_id"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
@@ -105,8 +100,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.integer "question_id"
     t.integer "option_id"
     t.boolean "correct"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "score", default: 0, null: false
   end
 
@@ -116,8 +111,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.boolean "winner"
     t.integer "score"
     t.string "comment"
-    t.datetime "created_at", precision: nil, default: "2021-12-24 12:55:04", null: false
-    t.datetime "updated_at", precision: nil, default: "2021-12-24 12:55:04", null: false
+    t.datetime "created_at", default: "2021-12-24 12:01:17", null: false
+    t.datetime "updated_at", default: "2021-12-24 12:01:17", null: false
     t.integer "actor_id", null: false
   end
 
@@ -126,23 +121,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.integer "weight", default: 0
     t.string "text"
     t.boolean "correct"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "survey_participant", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "survey_questions", force: :cascade do |t|
     t.integer "survey_id"
     t.string "text"
     t.string "description"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "survey_surveys", force: :cascade do |t|
@@ -153,8 +148,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.boolean "active", default: false
     t.integer "winning_score", default: 0
     t.integer "survey_type", default: 0
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "survey_stage"
     t.bigint "folder_id"
     t.date "archived_on"
     t.integer "user_id"
@@ -168,46 +164,48 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_111354) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "confirmation_token"
-    t.datetime "confirmed_at", precision: nil
-    t.datetime "confirmation_sent_at", precision: nil
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.integer "permission"
     t.string "invitation_token"
-    t.datetime "invitation_created_at", precision: nil
-    t.datetime "invitation_sent_at", precision: nil
-    t.datetime "invitation_accepted_at", precision: nil
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
     t.integer "invitation_limit"
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
-    t.integer "account_id"
-    t.boolean "email_enabled", default: true
     t.boolean "active", default: true, null: false
     t.date "deactivated_on"
-    t.integer "role", default: 0
-    t.string "phone"
+    t.boolean "email_enabled", default: true
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.index ["account_id"], name: "index_users_on_account_id"
+    t.integer "role", default: 0
+    t.string "phone"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id", name: "active_storage_attachments_blob_id_fkey"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id", name: "active_storage_variant_records_blob_id_fkey"
-  add_foreign_key "folders", "accounts"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "folders", "spaces"
   add_foreign_key "folders", "users"
   add_foreign_key "pinned_spaces", "spaces"
   add_foreign_key "pinned_spaces", "users"
   add_foreign_key "recent_surveys", "survey_surveys", column: "survey_surveys_id"
   add_foreign_key "recent_surveys", "users"
-  add_foreign_key "spaces", "accounts"
   add_foreign_key "spaces", "users"
   add_foreign_key "survey_surveys", "folders"
 end
