69100e8596476112f879ae830c4cd3ba33100d5b
hansika17 <hansika.kalra171@gmail.com>
2023-09-19 20:00:19 +0530

    migration updated

--- db/migrate/20230919074108_backfill_accounts.rb
+++ db/migrate/20230919074108_backfill_accounts.rb
@@ -1,8 +1,15 @@
 class BackfillAccounts < ActiveRecord::Migration[7.0]
   def change
     account = Account.create(name: "Crownstack")
-    User.all.each do |user|
-      user.update(account_id: account.id)
+    Account.reset_column_information  # Ensure that the Account model is aware of the current schema
+
+    default_account_id = account.id
+    ActsAsTenant.without_tenant do
+      # Update the account_id where it is null
+      User.where(account_id: nil).update_all(account_id: default_account_id)
+      Space.where(account_id: nil).update_all(account_id: default_account_id)
+      Survey::Survey.where(account_id: nil).update_all(account_id: default_account_id)
+      Survey::Attempt.where(account_id: nil).update_all(account_id: default_account_id)
     end
   end
 end
