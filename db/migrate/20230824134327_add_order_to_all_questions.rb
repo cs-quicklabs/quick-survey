77e1fb0d8923a59cfa34bf2b5122fee22e0f6486
Aashish Dhawan <aashishdhawan@gmail.com>
2023-10-01 01:06:51 +0530

    remove migratio

--- db/migrate/20230824134327_add_order_to_all_questions.rb
+++ db/migrate/20230824134327_add_order_to_all_questions.rb
@@ -1,12 +1,11 @@
 class AddOrderToAllQuestions < ActiveRecord::Migration[7.0]
   def change
     ActsAsTenant.without_tenant do
-
-    Survey::Survey.all.each do |survey|
-      survey.questions.order(:created_at).each_with_index do |question, index|
-        question.update(order: index + 1)
+      Survey::Survey.all.each do |survey|
+        survey.questions.order(:created_at).each_with_index do |question, index|
+          question.update(order: index + 1)
+        end
       end
     end
-  
   end
 end
