517981e4f81217918ab9b8aa21b95eabf3213247
Aashish Dhawan <aashishdhawan@gmail.com>
2023-10-01 01:21:04 +0530

    remove migratio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -128,7 +128,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "checklist.quicklabs.in",     # Allow requests from example.com
+    "screening.crownstack.app",     # Allow requests from example.com
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
