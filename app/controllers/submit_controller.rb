874826ae9f8bea8fa1ca5e006534880e9db491f7
hansika17 <hansika.kalra171@gmail.com>
2023-10-19 19:35:47 +0530

    Update submit_controller.rb

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -28,10 +28,8 @@ class SubmitController < BaseController
   end
 
   def submit
-    binding.irb
     authorize @attempt, :submit?
     @attempt.update_attribute("comment", params[:comment])
-    binding.irb
     redirect_to attempts_path, notice: "Thank you for submitting your survey."
   end
 
