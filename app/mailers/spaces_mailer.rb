2447a70ba4920c7b116435a70b6ccb98e0d226dd
Aashish Dhawan <aashishdhawan@gmail.com>
2023-11-04 22:34:34 +0530

    updated subject line

--- app/mailers/spaces_mailer.rb
+++ app/mailers/spaces_mailer.rb
@@ -3,20 +3,20 @@ class SpacesMailer < ApplicationMailer
     @actor = params[:actor]
     @user = params[:user]
     @space = params[:space]
-    mail(to: @user.email, subject: "A space has been created", template_path: "mailers/spaces_mailer")
+    mail(to: @user.email, subject: "Quick Survey: A space has been created", template_path: "mailers/spaces_mailer")
   end
 
   def archived_email
     @actor = params[:actor]
     @user = params[:user]
     @space = params[:space]
-    mail(to: @user.email, subject: "Space Archived", template_path: "mailers/spaces_mailer")
+    mail(to: @user.email, subject: "Quick Survey: Space Archived", template_path: "mailers/spaces_mailer")
   end
 
   def unarchived_email
     @actor = params[:actor]
     @user = params[:user]
     @space = params[:space]
-    mail(to: @user.email, subject: "Space Unarchived", template_path: "mailers/spaces_mailer")
+    mail(to: @user.email, subject: "Quick Survey: Space Unarchived", template_path: "mailers/spaces_mailer")
   end
 end
