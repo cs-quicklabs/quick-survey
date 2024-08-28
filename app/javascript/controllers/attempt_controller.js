683a8360db7fec16c6a5c21d764e834e0a7228e2
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-28 13:20:11 +0530

    code refactoring

--- app/javascript/controllers/attempt_controller.js
+++ app/javascript/controllers/attempt_controller.js
@@ -7,7 +7,7 @@ export default class extends Controller {
     event.preventDefault();
 
     const url =
-    "/"+event.target.dataset.accountId+ "/answer/"+ event.target.dataset.attemptId+"?question_id=" +
+      "/" + event.target.dataset.accountId + "/answer/" + event.target.dataset.attemptId + "?question_id=" +
       event.target.dataset.questionId +
       "&option_id=" +
       event.target.dataset.optionId
@@ -17,35 +17,35 @@ export default class extends Controller {
         this.attemptTarget.innerHTML = html;
       });
   }
-  scored(event)
-  {
-  event.preventDefault();
 
-  const url = "/"+
-  event.target.dataset.accountId+
-    "/score/"+ event.target.dataset.attemptId+"?question_id=" +
-    event.target.dataset.questionId +
-    "&option_id=" +
-    event.target.dataset.optionId+
-    "&score=" +
-    event.target.dataset.score
-  fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
-    .then((response) => response.text())
-    .then((html) => {
-      this.attemptTarget.innerHTML = html;
-    });
-}
-check(event)
-{
-  event.preventDefault();
-  const url = "/"+event.target.dataset.accountId+ "/check/"+ event.target.dataset.attemptId+"?question_id=" +
-    event.target.dataset.questionId +
-    "&option_id=" +
-    event.target.dataset.optionId
-  fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
-    .then((response) => response.text())
-    .then((html) => {
-      this.attemptTarget.innerHTML = html;
-    });
-}
+  scored(event) {
+    event.preventDefault();
+
+    const url = "/" +
+      event.target.dataset.accountId +
+      "/score/" + event.target.dataset.attemptId + "?question_id=" +
+      event.target.dataset.questionId +
+      "&option_id=" +
+      event.target.dataset.optionId +
+      "&score=" +
+      event.target.dataset.score
+    fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
+      .then((response) => response.text())
+      .then((html) => {
+        this.attemptTarget.innerHTML = html;
+      });
+  }
+
+  check(event) {
+    event.preventDefault();
+    const url = "/" + event.target.dataset.accountId + "/check/" + event.target.dataset.attemptId + "?question_id=" +
+      event.target.dataset.questionId +
+      "&option_id=" +
+      event.target.dataset.optionId
+    fetch(url, { headers: { Accept: "text/vnd.turbo-stream.html" } })
+      .then((response) => response.text())
+      .then((html) => {
+        this.attemptTarget.innerHTML = html;
+      });
+  }
 }
