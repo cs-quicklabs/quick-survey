f102912fd07207c6734bd959e3902213bce61716
Aashish Dhawan <aashishdhawan@gmail.com>
2024-11-15 22:39:08 +0530

    fixed minor bugs

--- app/mailers/spaces_mailer.rb
+++ app/mailers/spaces_mailer.rb
@@ -6,6 +6,13 @@ class SpacesMailer < ApplicationMailer
     mail(to: @user.email, subject: "Quick Survey: A space has been created", template_path: "mailers/spaces_mailer")
   end
 
+  def update_space_email
+    @actor = params[:actor]
+    @user = params[:user]
+    @space = params[:space]
+    mail(to: @user.email, subject: "Quick Survey: A space has been updated", template_path: "mailers/spaces_mailer")
+  end
+
   def archived_email
     @actor = params[:actor]
     @user = params[:user]
